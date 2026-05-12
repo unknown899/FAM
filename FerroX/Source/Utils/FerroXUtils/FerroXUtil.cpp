68a7912a114850ebc7a861dbf62f8631ac8c8417
unknown899 <kaiwy58.ee11@nycu.edu.tw>
2026-05-13 01:44:28 +0800

    修改FerroXUtil.cpp內的整個Contains_sc函是，這樣才能用gpu跑（詳情見：https://chatgpt.com/c/6a02f7b1-ea9c-83e9-9874-132721cd1fac）

--- FerroX/Source/Utils/FerroXUtils/FerroXUtil.cpp
+++ FerroX/Source/Utils/FerroXUtils/FerroXUtil.cpp
@@ -10,16 +10,16 @@ using namespace amrex;
 
 
 void FerroX_Util::Contains_sc(MultiFab& MaterialMask, bool& contains_SC)
-{
-    amrex::Print() << "ENTER Contains_sc\n";
+{    
+	amrex::Print() << "ENTER Contains_sc\n";
 
-    amrex::Print() << "ok = "
-                   << MaterialMask.ok() << "\n";
+	amrex::Print() << "ok = "
+		   << MaterialMask.ok() << "\n";
 
-    amrex::Print() << "nComp = "
-                   << MaterialMask.nComp() << "\n";
+	amrex::Print() << "nComp = "
+		   << MaterialMask.nComp() << "\n";
 
-    amrex::Print() << "nBox = "
-                   << MaterialMask.boxArray().size() << "\n";
-    contains_SC = (MaterialMask.max(0) >= 2.0);
+	amrex::Print() << "nBox = "
+		   << MaterialMask.boxArray().size() << "\n";
+	contains_SC = (MaterialMask.max(0) >= 2.0);
 }
